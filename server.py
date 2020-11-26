c0db2d6e252245387012ea51d6ab12006a7cd6c9
root <grebenkinvalik123@gmail.com>
2020-11-26 10:23:51 +0200

    files

---
+++ server.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+
+USERS = set()
+
+async def addUser(websocket):
+    USERS.add(websocket)
+
+async def removeUser(websocket):
+    USERS.remove(websocket)
+
+async def socket(websocket, path):
+    await addUser(websocket)
+
+    try:
+        while True:
+            message = await websocket.recv()
+            
+            await asyncio.wait([user.send(message) for user in USERS])
+    finally:
+        await removeUser(websocket)
+
+start_server = websockets.serve(socket, '0.0.0.0', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
